50984884ba693136be86e8025425e798ef6f8de6
MistaRae <itsamistarae@gmail.com>
2021-05-05 17:22:41 +0100

    4th test passing

--- models/traveller.js
+++ models/traveller.js
@@ -17,6 +17,9 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance
+  })
 
 };
 
